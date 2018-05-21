6fe7b7a14a81f2d85feff093473c09906c16b153
Administrator <Administrator@lb_chen>
2018-05-21 17:25:08 +0800

    增加一个IP过滤器

---
+++ beini-gateway/src/main/java/com/beini/gateway/filter/IPFilter.java
@@ -0,0 +1,34 @@
+package com.beini.gateway.filter;
+
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+/**
+ * IP过滤器
+ * @author lb_chen
+ *
+ */
+//@Component
+public class IPFilter extends ZuulFilter{
+
+	@Override
+	public boolean shouldFilter() {
+		return false;
+	}
+
+	@Override
+	public Object run() {
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return null;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+}
